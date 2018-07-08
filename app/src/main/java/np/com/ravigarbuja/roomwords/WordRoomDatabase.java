393160a7ccc320788fe4bfbdb2617e5a0e183931
pfieffer <cbpmgr@gmail.com>
2018-07-08 13:07:20 +0545

    Implement RoomDatabase which describes the Entity(Table) and returns a single instance of database at a time

---
+++ app/src/main/java/np/com/ravigarbuja/roomwords/WordRoomDatabase.java
@@ -0,0 +1,33 @@
+package np.com.ravigarbuja.roomwords;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Word.class}, version = 1)
+public abstract class WordRoomDatabase extends RoomDatabase {
+
+    public abstract WordDao wordDao();
+
+    private static WordRoomDatabase INSTANCE;
+
+
+    //Define the DAOs that work with the database. Provide an abstract "getter" method for each @Dao.
+    static WordRoomDatabase getDatabase(final Context context) {
+
+        //Make the WordRoomDatabase a singleton to prevent having multiple instances of the database opened at the same time.
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    //creating database
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database")
+                            .build();
+
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
